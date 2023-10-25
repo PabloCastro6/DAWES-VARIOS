6f5c01be714d64773d7983ea31fabeddf7b09e8f
Pablo Castro <alumnofp@FPB23.CALASANZSALAMANCA.local>
2023-10-25 13:58:23 +0200

    Saludo

---
+++ DAWES-varios/tema1/Saludo/src/main/java/com/pablo/saludo/controllers/HolaController.java
@@ -0,0 +1,49 @@
+package com.pablo.saludo.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/diHola")
+/**
+ * Servlet implementation class HolaController
+ */
+public class HolaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nombre = request.getParameter("nombre");
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		out.print("<html> <body> " );
+		out.print("<h1> hola " + nombre + "</h1>");
+		out.print("</body> </html> ");
+	}
+
+}
