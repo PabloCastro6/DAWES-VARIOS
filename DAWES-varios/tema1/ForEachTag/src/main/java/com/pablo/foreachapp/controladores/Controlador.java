ab65e5be648fac416a0677c74aa6cca911ae4b3e
Pablo Castro <alumnofp@FPB23.CALASANZSALAMANCA.local>
2023-11-14 10:04:59 +0100

    ForEachTag

---
+++ DAWES-varios/tema1/ForEachTag/src/main/java/com/pablo/foreachapp/controladores/Controlador.java
@@ -0,0 +1,49 @@
+package com.pablo.foreachapp.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pablo.foreachapp.bean.Persona;
+
+@WebServlet("/obtenpersonas")
+public class Controlador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controlador() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Persona> listaPersonas = new ArrayList<>();
+		
+		listaPersonas.add( new Persona("Pepe", 2000));
+		listaPersonas.add(new Persona("kike",100000));
+		listaPersonas.add(new Persona("Juanita",5000));
+		listaPersonas.add(new Persona("Pablo",3000));
+		
+		request.setAttribute("personas", listaPersonas);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ DAWES-varios/tema1/ForEachTag/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+Pulse <a href="http://localhost:8080/ForEachTag/obtenpersonas">aqui</a> para obtener la lista de personas
+</body>
+</html>
